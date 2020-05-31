e9f7991295b0e9f976792abf82d393b38fbf862b
thomasdriscoll <thomasdriscoll98@gmail.com>
2020-05-30 22:20:08 -0500

    merge junk commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router,
-        Switch,
-        Route } from 'react-router-dom';
-import NavigationBar from './components/molecules/NavigationBar';
 import './stylesheets/App.scss';
 import './stylesheets/NavigationBar.scss';
 
-
-//Router tutorial: https://reacttraining.com/react-router/web/guides/quick-start
 function App() {
   return (
-    <Router>
-      <div>
-      <NavigationBar />
-      <Switch>
-        <Route path="/projects">
-            <About />
-        </Route>
-        <Route path="/blog">
-            <Users />
-        </Route>
-        <Route path="/">
-            <Home />
-        </Route>
-    </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>Projects</h2>;
-}
-
-function Users() {
-  return <h2>Blog</h2>;
 }
 
 export default App;
